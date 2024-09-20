e99f4442046512817d9a3c3a81f2c93ca9d0ab62
pacheco90 <pedropachecoasencio90@gmail.com>
2024-09-19 22:05:39 -0600

    :sparkles: add functionalities to javascript file

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,37 @@
-// let btn = document.querySelector('#btn');
+// let num1 = 5;
+// let num2 = 5;
 
-// btn.addEventListener('click', function(){
-//     alert("Hello");
-//     alert('nestor');
-// });
 
+// if(num1<=num2){
+//     alert("Si es Menor o igual que el numero 1");
+// }else{
+//     alert("No es Menor");
+// };
+
+
+// = asignacion
+// == comparacion
+// === comparacion de tipo
+// "<" Menor que
+// ">" Mayor que
+// ">=" Mayor o igual que
+// "<=" Menor o igual que
+
+
+
+// console.log(lanzador);
+
+const open = document.querySelector(".nav-btn");
+const navigator = document.querySelector(".nav1");
+const close = document.querySelector(".close");
+
+console.log(navigator);
+
+open.addEventListener("click", function(){
+    navigator.style.display = "flex";
+});
+
+
+close.addEventListener("click", function(){
+    navigator.style.display = "none";
+});
