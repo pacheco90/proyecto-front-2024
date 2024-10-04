dd88133d7e80e5ffd59afd073fc9ff43e2268a45
pacheco90 <pedropachecoasencio90@gmail.com>
2024-10-03 20:52:24 -0600

    :bug: add new optimization into navigation bar

--- src/js/script.js
+++ src/js/script.js
@@ -20,22 +20,57 @@
 
 
 // console.log(lanzador);
+
+// //Primera Funcionalidad del navegador
+
+// //Elemento afectado relativo a las acciones
+// const navigator = document.querySelector(".nav1");
+
+
+// //Acciones que afectan al navigator
+// const open = document.querySelector(".nav-btn");
+// const close = document.querySelector(".close-nav");
+// const out = document.querySelector("main");
+
+
+
+// open.addEventListener("click", function(){
+//     navigator.classList.add('show');
+// });
+
+// close.addEventListener("click", function(){
+//     navigator.classList.remove('show');
+// });
+
+// out.addEventListener("click", function(){
+//     navigator.classList.remove('show');
+// });
+
+
+
+//Segunda funcionalidad
+
+//Elemento afectado relativo a las acciones
 const navigator = document.querySelector(".nav1");
 
+
+//Acciones que afectan al navigator
 const open = document.querySelector(".nav-btn");
 const close = document.querySelector(".close-nav");
 const out = document.querySelector("main");
 
 
-
-open.addEventListener("click", function(){
+//Funcionalidades que permiten mostrar o esconder el navigator
+const showNavigator = function(){
     navigator.classList.add('show');
-});
+};
 
-close.addEventListener("click", function(){
+const hiddeNavigator = function(){
     navigator.classList.remove('show');
-});
+};
 
-out.addEventListener("click", function(){
-    navigator.classList.remove('show');
-});
+
+//Escucha los clicks los elementos del dom para afectar el navigator por medio de la funcion
+open.addEventListener("click", showNavigator);
+close.addEventListener("click", hiddeNavigator);
+out.addEventListener("click", hiddeNavigator);
